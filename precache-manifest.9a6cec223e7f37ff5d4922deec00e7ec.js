646ea41c2f3de519379ba544c4927119f3ba21eb
Willyanfx <willyan.fx@gmail.com>
2020-03-28 17:20:37 -0700

    Updates

---
+++ precache-manifest.9a6cec223e7f37ff5d4922deec00e7ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4faf8feacd3075c4d298651657e94d1",
+    "url": "/cle/index.html"
+  },
+  {
+    "revision": "b835a4f28150aaa91936",
+    "url": "/cle/static/css/2.26fa88a6.chunk.css"
+  },
+  {
+    "revision": "d7f80050ba960c3d195f",
+    "url": "/cle/static/css/main.fd119d29.chunk.css"
+  },
+  {
+    "revision": "b835a4f28150aaa91936",
+    "url": "/cle/static/js/2.dbaa36e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cle/static/js/2.dbaa36e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f80050ba960c3d195f",
+    "url": "/cle/static/js/main.4fc874b4.chunk.js"
+  },
+  {
+    "revision": "f10b3197269c77ea4480",
+    "url": "/cle/static/js/runtime-main.6ecc345f.js"
+  }
+]);
