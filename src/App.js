e2fb9c2480acdf0c8b0e86db140aa4ed76ddea2c
willyanfx <willyan.fx@gmail.com>
2022-07-20 11:14:35 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import styles from './App.module.scss';
-import { AppStateProvider } from './appState';
+import styles from "./App.module.scss";
+import { AppStateProvider } from "./appState";
 
 import { Layout } from "./components/Layout";
 
 function App() {
-	return (
-		<div className={styles.App}>
-				<AppStateProvider>
-					<Layout />
-				</AppStateProvider>
-		</div>
-	);
+  return (
+    <div className={styles.App}>
+      <AppStateProvider>
+        <Layout />
+      </AppStateProvider>
+    </div>
+  );
 }
 
 export default App;
