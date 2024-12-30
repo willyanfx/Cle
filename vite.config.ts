2fc2a414309360a534e562be59fd0cec9089d33c
willyanfx <willyan.fx@gmail.com>
2024-12-29 17:05:58 -0700

    vite + react 19

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/cle/",
+    plugins: [react()],
+    base: '/cle/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
